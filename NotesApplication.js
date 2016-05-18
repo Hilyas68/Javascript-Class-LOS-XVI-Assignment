91729581cd86d2267322990a81270d9f40711fcb
Liasu Hassan <htope68@gmail.com>
2016-05-18 09:20:38 +0100

    documented my code with comments

--- NotesApplication.js
+++ NotesApplication.js
@@ -22,7 +22,7 @@ var NotesApplication = function (autor) {
   	console.log('By Author ' + autor);
   	
   }
-  
+  //get Note ID from a list
   this.getNote_id = function(note_id){
   	this.note_id = notes.indexOf(this.note_content);
   	
@@ -34,16 +34,19 @@ var NotesApplication = function (autor) {
   	 console.log('By Author ' + autor);
   }
   
+  //Searching for items list content 
   this.search = function(search_text){
   	this.search_text = notes.search(search_text);
   	return this.search_text
   }	
   
+  //deleting item from the list
   this.delete = function(note_id){
   	this.note_id = notes.indexOf(note_content);
   	delete notes[this.note_id];
   }
   
+  //editing a item list 
   this.edit = function(note_id, new_content){
   	this.note_id = notes.indexOf(note_content);
    notes[this.note_id] = new_content
